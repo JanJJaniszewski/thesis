543eeab5c46554a48232bc211733a2e1782dabf0
Jan Janiszewski <pole19901990@gmail.com>
2023-03-21 20:25:44 +0100

    added gan

--- src/thesis/pipelines/data_processing/gan_baseline.py
+++ src/thesis/pipelines/data_processing/gan_baseline.py
@@ -32,59 +32,22 @@ from sklearn.metrics import roc_auc_score, precision_score, recall_score, f1_sco
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-%load_ext autoreload
-%autoreload 2
-pd.set_option('display.float_format', lambda x: '%.5f' % x)
-# Load the freMTPL2freq dataset
-train_data = pd.read_pickle('Data/Throughput/MTPL_sev_train.pickle')
-test_data = pd.read_pickle('Data/Throughput/MTPL_sev_test.pickle'# Transforming into bins
-column_trans = ColumnTransformer(
-    [
-        ("passthrough_numeric",
-         "passthrough",
-         ["BonusMalus", 'Density', 'VehAge', 'DrivAge', 'Area', 'ClaimAmount']),
-        (
-            "onehot_categorical",
-            OneHotEncoder(),
-            ["VehBrand", "VehPower", "VehGas", "Region"],
-        )
-    ],
-    remainder="drop",
-)
-Xy = column_trans.fit_transform(train_data)
-Xy = pd.DataFrame.sparse.from_spmatrix(Xy)
-Xy.columns = [f'Col{x}' for x in Xy.columns]
-Xy.head()
+def train_gan_baseline(train_data, parameters):
+    nums = parameters['numeric_features']
+    cats = parameters['categorical_features']
 
+    column_trans = ColumnTransformer(
+        [
+            ("passthrough_numeric", "passthrough", nums),
+            ("onehot_categorical", OneHotEncoder(sparse=False), cats)
+        ],
+        remainder="drop"
+    )
 
-# Define the hyperparameters for the CTGAN GAN
-param_grid = {'epochs': [100, 200, 300],
-              'gen_dim': [(128, 128), (256, 256), (512, 512)],
-              'dis_dim': [(128, 128), (256, 256), (512, 512)],
-              'batch_norm': [True, False],
-              'batch_size': [500, 1000, 2000]}
+    Xy = column_trans.fit_transform(train_data)
 
-# Define the CTGAN GAN object
-ctgan = CTGAN(verbose=False, cuda=True, epochs=2)
-ctgan.fit(Xy)
-ctgan.sample(2)
-# Perform GridSearchCV to tune hyperparameters
-#gs = GridSearchCV(estimator=ctgan, param_grid=param_grid, cv=5, n_jobs=-1, scoring='neg_mean_absolute_error')
-
-
-# Generate new synthetic data using the best hyperparameters
-#ctgan.set_params(**gs.best_params_)
-#ctgan.fit(train_data)
-#synthetic_data = ctgan.sample(len(test_data))
-
-# Compute evaluation metrics
-#print('AUC:', roc_auc_score(test_data['ClaimNb'], synthetic_data['ClaimNb']))
-#print('Precision:', precision_score(test_data['ClaimNb'], synthetic_data['ClaimNb']))
-#print('Recall:', recall_score(test_data['ClaimNb'], synthetic_data['ClaimNb']))
-#print('F1 score:', f1_score(test_data['ClaimNb'], synthetic_data['ClaimNb']))
-
-# Plot the distribution of the original and synthetic data
-#sns.histplot(data['ClaimNb'], kde=True, color='blue', alpha=0.5, label='Original')
-#sns.histplot(synthetic_data['ClaimNb'], kde=True, color='orange', alpha=0.5, label='Synthetic')
-#plt.legend()
-#plt.show()
+    # Define the CTGAN GAN object
+    ctgan = CTGAN(verbose=False, cuda=True, epochs=2)
+    ctgan.fit(Xy)
+    ctgan.save('data/07_models/gan_baseline_sev.pickle')
+    return ctgan
--- src/thesis/pipelines/data_processing/pipeline.py
+++ src/thesis/pipelines/data_processing/pipeline.py
@@ -4,21 +4,26 @@ generated using Kedro 0.18.4
 """
 
 from .dataprep_common import *
-from .regression_baseline import *
+
 from kedro.pipeline import Pipeline, node, pipeline
 
-
+from .regression_baseline import *
+from .gan_baseline import *
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
             # Load and save everything first in the database
             node(func=load_data_and_save,
-                 inputs = None,
+                 inputs=None,
                  outputs=['raw_frequency', 'raw_severity'],
                  name='load_external_dataset'),
             node(func=common_dataprep,
                  inputs=['raw_frequency', 'raw_severity'],
-                 outputs = ['freq_train', 'sev_train', 'all_train', 'freq_test', 'sev_test', 'all_test'],
+                 outputs=['freq_train', 'sev_train', 'all_train', 'freq_test', 'sev_test', 'all_test'],
                  name='common_data_prep'),
+            node(func=train_gan_baseline,
+                 inputs='sev_train',
+                 outputs='gan_baseline_sev',
+                 name='train_gan_baseline'),
         ]
     )
